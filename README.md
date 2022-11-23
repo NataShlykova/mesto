dfa24941c4a6aedb2650dfcf26dfc069a874cae1
Natalya Shlykova <natalie.shlykova@yandex.ru>
2022-11-23 21:11:15 +0500

    3 проверка

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # Проект: Место
 
+## Описание проекта
+
+Одностраничный, адаптивный сайт, который создан с использованием HTML, CSS и JavaScript.
+Реализован по правилам flexbox, grid, медиазапросы, подключены шрифты. Форма редактирования профиля на JavaScript.
+
+## Технологии
+* HTML 5;
+* CSS 3;
+* JavaScript;
+
 ![Гифка с Gifius ru](https://user-images.githubusercontent.com/113706933/203549252-a785a898-296a-4792-befe-4ce60896c10a.gif)
 
-* Проект [Место](https://natashlykova.github.io/mesto/index.html), доступен.
+* Посмотреть проект можно [тут](https://natashlykova.github.io/mesto/index.html).
 
 * [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,23 @@
 const openEditButton = document.querySelector('.profile__edit-button');
 const popupElement = document.querySelector('.popup');
 const closePopupButton = document.querySelector('.popup__close-button');
-const nameForm = document.querySelector('.popup__input_name');
-const workForm = document.querySelector('.popup__input_work');
+const nameForm = document.querySelector('.popup__input_string_name');
+const workForm = document.querySelector('.popup__input_string_work');
 const inputForm = document.querySelector('.popup__form');
-const formSubmit = document.querySelector('.popup__submit');
 const profileTitle = document.querySelector('.profile__title');
 const profileWork = document.querySelector('.profile__subtitle');
 
 
 
 function openPopup () {
-    popupElement.classList.add('popup_open');
+    popupElement.classList.add('popup_opened');
     nameForm.value = profileTitle.textContent;
     workForm.value = profileWork.textContent;
 }
 
 
 function closePopup () { 
-   popupElement.classList.remove('popup_open');
+   popupElement.classList.remove('popup_opened');
 };
 
 function formSubmitHandler (evt) {
