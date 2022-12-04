4b2dda2568a4fb7be1dd9ad0d448d2775d7500f8
Natalya Shlykova <natalie.shlykova@yandex.ru>
2022-12-04 18:21:48 +0500

    исправить наименование методов и функций, добавить плавное открытие попапам перенести все константы в начало файла

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,23 @@
 const popupButtonEdit = document.querySelector('.popup_button_edit'); // попап редактирования профиля
 const openEditButton = document.querySelector('.profile__edit-button'); // кнопка редактирования профиля
-const closePopupButton = document.querySelector('.popup__close-button'); // кнопка закрытия редактирования профиля
-
+const profileCloseButton = document.querySelector('.popup__close-button'); // кнопка закрытия редактирования профиля
 const nameForm = document.querySelector('.popup__input_string_name'); // поле имени в попапе редактирования профиля
 const workForm = document.querySelector('.popup__input_string_work'); // поле деятельности в попапе редактирования профиля
 const inputForm = document.querySelector('.popup__form'); // форма попапа редактирования профиля
 const profileTitle = document.querySelector('.profile__title'); // имя в профиле на странице
 const profileWork = document.querySelector('.profile__subtitle'); // название деятельности на странице
-
 const openAddButton = document.querySelector('.profile__add-button'); // кнопка добавления изображений на странице 
 const popupAddButton = document.querySelector('.popup_add_button'); // попап добавления изображений 
 const popupCloseAddButton = document.querySelector('.popup__close-button-add'); // кнопка закрытия попапа добавления изображений
-
 const popupImage =document.querySelector('.popup_image'); //попап  изображения
 const popupCloseButtonZoom = document.querySelector('.popup__close-button-zoom'); // кнопка закрытия увеличенного изображения
-
-
 const sectionElements = document.querySelector('.elements'); //контейнер с изображениями на странице
 const imageElements = document.querySelector('#image-elements'); // клонирование изображений
 const imgAddForm = popupAddButton.querySelector('.popup__form');//форма попапа добавления изображений
-
+const elementParagraph = document.querySelector ('.popup__paragraph');// текст в попапе увеличенного изображения
+const popupBigImage = document.querySelector ('.popup__img'); // увеличенное изображение в попапе
+const strigNamePhoto = document.querySelector ('.popup__input_string_name-photo'); // поле ввода наименования места в попапе добавления изображения
+const stringLinkPhoto = document.querySelector ('.popup__input_string_link'); //поле ввода ссылки в попапе добавления изображения
 const initialElements = [  // изображения в массиве
     {
       name: 'БАО Алматы',
@@ -46,129 +44,111 @@ const initialElements = [  // изображения в массиве
       link: './images/6images/чимбулак.jpg'
     }
   ];
-
-
 //фунция открытия попапа 
-const openPopupList = (openPopup) => {
+const openPopup = (openPopup) => {
   openPopup.classList.add('popup_opened');  
 };
 
 //функция закрытия попапа 
-const closePopupList = (closePopup) => { 
+const closePopup = (closePopup) => { 
   closePopup.classList.remove('popup_opened');
  };
 
 // функция обработчик формы попапа редактирования профиля
-function formSubmitHandler (evt) {
+function handleProfileFormSubmit (evt) {
     evt.preventDefault();
     profileTitle.textContent = nameForm.value;
     profileWork.textContent = workForm.value;
-    closePopupList(popupButtonEdit);
+    closePopup (popupButtonEdit);
 }
 
 // функция для иnпутов
-function profileInput () {
+function fillProfileInputs () {
   nameForm.value = profileTitle.textContent;
   workForm.value = profileWork.textContent;
 }
 
-// функция преобразования добавления изображений 
-function elementAddButton (item) {
+// функция преобразования добавления изображений
+function createCard (item) {
   const elementPhoto = imageElements.content.cloneNode(true);
   const elementText = elementPhoto.querySelector ('.element__text');
   elementText.textContent = item.name;
-
   const elementImage = elementPhoto.querySelector ('.element__image');
   elementImage.src = item.link;
-
+  elementImage.alt = item.name;
   const elementButtonDelete = elementPhoto.querySelector ('.element__button-delete');
   elementButtonDelete.addEventListener ('click', deleteElementPhoto);
-
   const elementButtonLike = elementPhoto.querySelector ('.element__button');
-  elementButtonLike.addEventListener ('click', deleteElementButtonLike);
-
+  elementButtonLike.addEventListener ('click', toggleLike);
   elementImage.addEventListener ('click', function () {
    openPopupBigImage (item);
   });
-  
-  popupCloseButtonZoom.addEventListener ('click', function () {
-    closePopupList(popupImage);
-  });
-
   return elementPhoto;
-  
 };
 
 //функция открытия большого изображения
 function openPopupBigImage (data) {
-  const elementParagraph = document.querySelector ('.popup__paragraph');
-  elementParagraph.textContent =  data.name;
-
-  const popupBigImage = document.querySelector ('.popup__img');
-  popupBigImage.src = data.link;
-
-  openPopupList (popupImage);
+ elementParagraph.textContent =  data.name;
+ popupBigImage.src = data.link;
+ popupBigImage.alt = data.name;
+ openPopup (popupImage);
 }
 
 //функция удаления изображений
 function deleteElementPhoto (evt) {
-  evt.target.closest ('.element').remove();
+ evt.target.closest ('.element').remove();
 }
 
 // функция для лайка
-function deleteElementButtonLike (evt) {
+function toggleLike (evt) {
   evt.target.classList.toggle('element__button_active');
 }
 
 //функция добавления новых изображений в начало секции 
-function elementRender () {
-  const element = initialElements.map(function (item) {
-  const newElement = elementAddButton (item);
+function renderInitialCards () {
+  const elements = initialElements.map(function (item) {
+  const newElement = createCard (item);
   return newElement;  
   })
-  sectionElements.append (...element);
+  sectionElements.append (...elements);
 }
 
-function elementAdd (evt) {
+function addElement (evt) {
   evt.preventDefault();
-
-  const strigNamePhoto = document.querySelector ('.popup__input_string_name-photo');
-  const stringLinkPhoto = document.querySelector ('.popup__input_string_link');
-
   const newStringNamePhoto = strigNamePhoto.value;
   const newStringLinkPhoto = stringLinkPhoto.value;
-
-  const newElement = elementAddButton({name: newStringNamePhoto, link: newStringLinkPhoto});
-
+  const newElement = createCard ({name: newStringNamePhoto, link: newStringLinkPhoto});
   sectionElements.prepend(newElement);
-
   imgAddForm.reset ();
-  closePopupList (popupAddButton);
+  closePopup (popupAddButton);
 };
 
 //вызовы функций открытия-закрытия
 openEditButton.addEventListener('click', function() {
-  profileInput ();
-  openPopupList (popupButtonEdit)
+  fillProfileInputs ();
+  openPopup (popupButtonEdit)
+});
+profileCloseButton.addEventListener ('click', function () {
+  closePopup (popupButtonEdit)
 });
 
-closePopupButton.addEventListener ('click', function () {
-  closePopupList(popupButtonEdit)
-});
-
-inputForm.addEventListener ('submit', formSubmitHandler);
+inputForm.addEventListener ('submit', handleProfileFormSubmit);
 
 openAddButton.addEventListener ('click', function () {
-  openPopupList (popupAddButton)
+  openPopup (popupAddButton)
 });
 
 popupCloseAddButton.addEventListener ('click', function () {
-  closePopupList (popupAddButton)
+  closePopup (popupAddButton)
 });
 
-elementRender ();
-imgAddForm.addEventListener('submit', elementAdd);
+renderInitialCards ();
 
+imgAddForm.addEventListener('submit', addElement);
+
+popupCloseButtonZoom.addEventListener ('click', function () {
+  closePopup (popupImage);
+});
 
 
 
